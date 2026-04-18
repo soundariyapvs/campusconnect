214ee85c7f504ba06b384efdc77075d6af3a5195
soundariyapvs <soundariyapvs@gmail.com>
2026-04-18 12:24:24 +0530

    fix: update backend URL to render

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://campusconnect-backend-2trt.onrender.com',
 });
 
 // Request interceptor to inject the JWT token
